fa95740ab4d7443641e9997b50a9ab5fca869409
FeiCoding <254356151@qq.com>
2018-06-06 13:43:59 +0100

    Valid Parentheses

---
+++ (#20)ValidParentheses/ValidParentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        char ch[] = s.toCharArray();
+        for(char c : ch){
+            if(c == '(') st.push(')');
+            else if(c == '[') st.push(']');
+            else if(c == '{') st.push('}');
+            
+            else if(st.isEmpty() || st.pop() != c)
+                return false;
+        }
+        
+        return st.isEmpty();
+    }
+}
