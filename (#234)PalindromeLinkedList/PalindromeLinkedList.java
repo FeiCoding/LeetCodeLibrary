f4e82bcc75e0b58b7d28a1516d6c76b0d6d1fe6e
FeiCoding <254356151@qq.com>
2018-06-17 13:32:39 +0100

    234. Palindrome Linked List

---
+++ (#234)PalindromeLinkedList/PalindromeLinkedList.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null)
+            slow = slow.next;
+        slow = reverse(slow);
+        fast = head;
+        while(slow != null){
+            if(fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+        
+    }
+    public ListNode reverse(ListNode head){
+        ListNode pre = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
