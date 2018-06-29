7bb69404775f940d41c5f622364a6ac49c07f69d
FeiCoding <254356151@qq.com>
2018-06-29 17:11:59 +0100

    11. Container With Most Water

---
+++ (#11)ContainerWithMostWater/ContainerWithMostWater.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxAera = 0, left = 0, right = height.length - 1;
+        while(left < right){
+            maxAera = Math.max(maxAera, Math.min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right])
+                left++;
+            else right--;
+        }
+        return maxAera;
+    }
+}
