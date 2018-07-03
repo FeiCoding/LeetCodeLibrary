4d82bf62651612d88970d813ba7b859b99bd9be9
FeiCoding <254356151@qq.com>
2018-07-03 16:59:07 +0100

    2. Add Two Numbers

---
+++ (#2)AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode res = new ListNode(0);
+        ListNode point = res;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            if(l1 != null){
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            point.next = new ListNode(carry % 10);
+            carry /= 10;
+            point = point.next;
+        }
+        if(carry / 10 == 1)
+            point.next = new ListNode(1);
+        return res.next;
+    }
+}
