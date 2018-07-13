1e4faf43968e9ce50903d74168fea4fd2b8befce
FeiCoding <254356151@qq.com>
2018-07-13 12:42:25 +0100

    WordSearch

--- (#79)WordSearch/WordSearch.java
+++ (#79)WordSearch/WordSearch.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i ++){
             for(int j = 0; j < board[0].length; j ++){
-                return exist(board, word, i, j, 0);
+                if(exist(board, word, i, j, 0)) return true;
             }
         }
         return false;
