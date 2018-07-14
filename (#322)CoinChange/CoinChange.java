c2dd2de424e6b33fcc0519f8ebe4579bd6d744b3
FeiCoding <254356151@qq.com>
2018-07-14 11:43:00 +0100

    322. Coin Change

---
+++ (#322)CoinChange/CoinChange.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount < 0) return -1;
+        return coinChange(coins, amount, new int[amount]);
+    }
+    
+    private int coinChange(int[] coins, int rem, int []count){
+        if(rem < 0) return -1;
+        if(rem == 0) return 0;
+        if(count[rem - 1] != 0) return count[rem - 1];
+        int min = Integer.MAX_VALUE;
+        for(int coin : coins){
+            int res = coinChange(coins, rem - coin, count);
+            if(res >= 0 && res < min)
+                min = res + 1;
+        }
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
+    }
+}
